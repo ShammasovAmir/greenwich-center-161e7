097d96dac13af18dd1319248e900692eee150e0b
Stackbit Code Editor <projects@stackbit.com>
2021-11-09 15:13:50 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -58,7 +58,7 @@ export default class Body extends React.Component {
                     {_.get(this.props, 'data.config.favicon', null) && (
                     <link rel="icon" href={withPrefix(_.get(this.props, 'data.config.favicon', null))}/>
                     )}
-                    {/* Yandex.Metrika counter  */}>
+                    {/* Yandex.Metrika counter  */}
                     <script type="text/javascript" >
                     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
